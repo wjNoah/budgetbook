7fa27ece333a687ee9c6cfed0bde246866bfb5ec
Finz Noah <noah.finz@twinformatics.at>
2026-09-21 09:08:33 +0200

    inital commit

---
+++ src/main/java/com/example/budgetbook/BudgetbookApplication.java
@@ -0,0 +1,13 @@
+package com.example.budgetbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetbookApplication.class, args);
+	}
+
+}
